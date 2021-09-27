c9ca35d331f26c702b91a111e699154f1da176f6
schiafang <schiafang@gmail.com>
2021-09-27 17:10:50 +0800

    modify: source filesystem path

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `posts`,
-                path: `${__dirname}/content/posts`,
+                // path: `${__dirname}/content/posts`,
+                path: '../content/posts',
             },
         },
         {
